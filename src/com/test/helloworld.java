cfb5d8d25da80ef87f977f80ef864d18367e1436
zx060901 <540910349@qq.com>
2016-07-05 16:32:33 +0800

    Update helloworld.java

--- src/com/test/helloworld.java
+++ src/com/test/helloworld.java
@@ -4,7 +4,8 @@ public class helloworld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("helloworld!");
+		System.out.println("HelloWorld!");
+		System.out.println("HelloBaicai");
 	}
 
 }
